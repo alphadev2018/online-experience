2832c419ce94ac26e01430cdfedeabbe08174729
developer-lee <stari990430@gmail.com>
2020-11-09 18:43:45 +0900

    updates;

--- src/components/Home.js
+++ src/components/Home.js
@@ -589,7 +589,7 @@ class Home extends Component {
         this.transform.setSize(0.8);
 		this.transform.addEventListener( 'dragging-changed', function ( event ) { self.controls.enabled = ! event.value; } );
 
-		const ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.4 ); this.scene.add( ambientLight );
+		const ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.1 ); this.scene.add( ambientLight );
 		this.mainLight = new THREE.DirectionalLight( 0x9E9E9E, 1.0 ); this.scene.add( this.mainLight );
 		// this.mainLight = new THREE.DirectionalLight( 0xFFFFFF, 1.0 ); this.scene.add( this.mainLight );
 		this.mainLight.position.set(-50, 50, 50); this.mainLight.castShadow = true;
